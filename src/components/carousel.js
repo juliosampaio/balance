8dbcdf76977d1091488f827bb779e9c96666059f
Julio Sampaio <juliodssampaio@gmail.com>
2018-08-19 21:10:18 -0300

    Add carousel first version

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -4,45 +4,69 @@ export class Carousel {
     this.init();
   }
 
-  init() {}
+  init() {
+    this.addControls();
+  }
 
-  currentState() {
-    const active = this.wrapper.querySelector('.slide.active');
-    const next = active.nextElementSibling;
-    const successor = next && next.nextElementSibling;
-    const previous = active.previousElementSibling;
+  addControls() {
+    const controlsWrapper = this.wrapper.querySelector('.controls');
+    this.wrapper.querySelectorAll('.slide').forEach(slide => {
+      const control = document.createElement('a');
+      control.href = `#${slide.id}`;
+      control.classList.add('control');
+      if (slide.classList.contains('active')) {
+        control.classList.add('active');
+      }
+      control.addEventListener('click', () => {
+        controlsWrapper.querySelectorAll('a').forEach(c => c.classList.remove('active'));
+        control.classList.add('active');
+        this.moveTo(slide);
+      });
+      controlsWrapper.appendChild(control);
+    });
+  }
+
+  moveTo(slide) {
+    const active = this.getActive(slide);
+    const next = active && active.nextElementSibling;
+    const previous = active && active.previousElementSibling;
     const predecessor = previous && previous.previousElementSibling;
-    const state = { predecessor, previous, active, next, successor };
-    return state;
+    const successor = next && next.nextElementSibling;
+    this.hideAllPredecessors(predecessor);
+    this.removeStateClasses(predecessor).add('predecessor');
+    this.removeStateClasses(previous).add('previous');
+    this.removeStateClasses(active).add('active');
+    this.removeStateClasses(next).add('next');
+    this.removeStateClasses(successor).add('successor');
   }
 
-  next() {
-    const { predecessor, previous, active, next, successor } = this.currentState();
-    if (!active.nextElementSibling) {
-      return;
+  getActive(target) {
+    switch (target) {
+      case 'next':
+        return this.wrapper.querySelector('.active').nextElementSibling;
+      case 'prev':
+        return this.wrapper.querySelector('.active').previousElementSibling;
+      default:
+        return target;
     }
-    predecessor && predecessor.classList.replace('predecessor', 'hideLeft');
-    previous && previous.classList.replace('previous', 'predecessor');
-    active.classList.replace('active', 'previous');
-    next && next.classList.replace('next', 'active');
-    successor && successor.classList.replace('successor', 'next');
-    successor &&
-      successor.nextElementSibling &&
-      successor.nextElementSibling.classList.add('successor');
   }
 
-  prev() {
-    const { predecessor, previous, active, next, successor } = this.currentState();
-    if (!active.previousElementSibling) {
-      return;
+  hideAllPredecessors(predecessor) {
+    if (!predecessor) return;
+    let node = predecessor.previousElementSibling;
+    while (node && node.nodeType === Node.ELEMENT_NODE) {
+      node.classList.add('hideLeft');
+      node = node.previousElementSibling;
     }
-    predecessor &&
-      predecessor.previousElementSibling &&
-      predecessor.previousElementSibling.classList.replace('hideLeft', 'predecessor');
-    predecessor && predecessor.classList.replace('predecessor', 'previous');
-    previous && previous.classList.replace('previous', 'active');
-    active.classList.replace('active', 'next');
-    next && next.classList.replace('next', 'successor');
-    successor && successor.classList.remove('successor');
+  }
+
+  removeStateClasses(element) {
+    if (!element) {
+      return document.createElement('div').classList;
+    }
+    ['predecessor', 'previous', 'active', 'next', 'successor'].forEach(clazz =>
+      element.classList.remove(clazz),
+    );
+    return element.classList;
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,4 @@ import { Carousel } from './components/carousel';
 
 window.onload = () => {
   const c = new Carousel('balance-carousel');
-  console.log(c);
-  document.getElementById('prev').addEventListener('click', () => c.prev());
-  document.getElementById('next').addEventListener('click', () => c.next());
 };
